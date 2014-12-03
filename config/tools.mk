ae4b28aa25f3982c21f2efd6e6b75e76a31be2af
spezi77 <spezi7713@gmail.com>
2014-12-03 19:57:44 +0100

    Squashed commit: add Terminal.apk and use local path for copy files
    Change-Id: I7d89d382dd68cb864316216f90e73124d987b3b5

--- config/tools.mk
+++ config/tools.mk
@@ -1,13 +1,19 @@
+LOCAL_PATH:= vendor/slim/prebuilt/common
+
 # Utilize init.d scripts
 PRODUCT_COPY_FILES += \
-    vendor/slim/prebuilt/common/bin/sysinit:system/bin/sysinit \
-    vendor/slim/prebuilt/common/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
-    vendor/slim/prebuilt/common/etc/init.d/05mountext:system/etc/init.d/05mountext \
-    vendor/slim/prebuilt/common/etc/init.d/20extgapps:system/etc/init.d/20extgapps
+    $(LOCAL_PATH)/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
+    $(LOCAL_PATH)/etc/init.d/05mountext:system/etc/init.d/05mountext \
+    $(LOCAL_PATH)/etc/init.d/20extgapps:system/etc/init.d/20extgapps
 
 # Apps2sd files
 PRODUCT_COPY_FILES += \
-    vendor/slim/prebuilt/common/bin/a2sd:system/bin/a2sd \
-    vendor/slim/prebuilt/common/bin/fix_permissions:system/bin/fix_permissions \
-    vendor/slim/prebuilt/common/xbin/zipalign:system/xbin/zipalign \
-    vendor/slim/prebuilt/common/etc/init.d/10apps2sd-redux:system/etc/init.d/10apps2sd
+    $(LOCAL_PATH)/bin/a2sd:system/bin/a2sd \
+    $(LOCAL_PATH)/bin/fix_permissions:system/bin/fix_permissions \
+    $(LOCAL_PATH)/xbin/zipalign:system/xbin/zipalign \
+    $(LOCAL_PATH)/etc/init.d/10apps2sd-redux:system/etc/init.d/10apps2sd
+
+# Android Terminal Emulator
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/lib/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so \
+    $(LOCAL_PATH)/etc/jackpal/Term.apk:system/app/Term.apk
