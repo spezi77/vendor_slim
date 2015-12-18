caf16169ecb1f0ecb5c33305b19afcc0761971e1
spezi77 <spezi77@gmx.com>
2015-12-18 20:40:27 +0100

    Disable ADB authentication
    Change-Id: Ic824134c7ef973430cc7140de0ba71e1968b44aa

--- config/common.mk
+++ config/common.mk
@@ -24,6 +24,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.debug.alloc=0
 
+# Disable ADB authentication
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=0
+
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/slim/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
