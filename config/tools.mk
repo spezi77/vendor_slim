spezi77/android_vendor_liquid

Add LowSoundFixer as system apk (credits tytung)

products/tools.mk
@@ -18,3 +18,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/lib/libjackpal-androidterm5.so:system/lib/libjackpal-androidterm5.so \
     $(LOCAL_PATH)/lib/libjackpal-termexec2.so:system/lib/libjackpal-termexec2.so \
     $(LOCAL_PATH)/etc/jackpal/Term.apk:system/app/Term.apk
+
+# LowSoundFixer (credits tytung)
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/apk/LowSoundFixer.apk:system/app/LowSoundFixer.apk

spezi77/vendor_slim

Change-Id: Ida15a7a35c30cc4096dc72d2d6b185bb7d14d9ae
Signed-off-by: spezi77 <spezi77@gmx.com>

config/tools.mk
@@ -18,3 +18,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/lib/libjackpal-androidterm5.so:system/lib/libjackpal-androidterm5.so \
     $(LOCAL_PATH)/lib/libjackpal-termexec2.so:system/lib/libjackpal-termexec2.so \
     $(LOCAL_PATH)/etc/jackpal/Term.apk:system/app/Term.apk
+
+# LowSoundFixer (credits tytung)
+PRODUCT_COPY_FILES += \
+    vendor/slim/prebuilt/apk/LowSoundFixer.apk:system/app/LowSoundFixer.apk